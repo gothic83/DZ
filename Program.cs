9b9ed5df253b79ce4de0826a9f97c0d8c054d7c1
Dmitry <gothic83@example.com>
2022-04-13 21:20:58 +0400

    Готовое ДЗ

--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿Console.WriteLine("Hello, World!");
-git status
+﻿/*
+Задача №1
+Console.Write("Input num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input num2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+    { 
+        Console.WriteLine("max " + num1 );
+    }
+if (num1<num2)
+    {
+        Console.WriteLine("max "  + num2 );
+    }
+    
+ */
+/*
+ //Задача №2
+Console.Write("Input num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input num2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input num3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+    if (num2 > max)
+        max = num2;
+    if (num3 > max)
+        max = num3;
+    { 
+        Console.WriteLine("max " + max );
+    }
+
+*/
+   /* //  Задача №3
+Console.Write("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2==0)
+    {
+    Console.WriteLine("число " + num + " чётное ");
+    }
+else
+    { 
+    Console.WriteLine("число " + num + " не чётное ");
+    }
+*/
+
+/*
+//Задача №4
+Console.Write("Input num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+while (n < num)
+        
+    {if (n % 2==0)
+        Console.WriteLine(n);
+    n = n+1;
+    }
+
+*/
